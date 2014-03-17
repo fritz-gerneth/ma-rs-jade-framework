d90e8044b6867929fcbf93cdcc4b6878c22cabac
Fritz Gerneth <fgerneth@effms.de>
2014-03-17 18:23:33 +0100

    [de.effms.jade.commons] PubSubAgent for better typehinting

---
+++ common/src/main/java/de/effms/jade/agent/PubSubAgent.java
@@ -0,0 +1,8 @@
+package de.effms.jade.agent;
+
+import de.effms.jade.agent.publish.PublishingAgent;
+import de.effms.jade.agent.subscribe.SubscribingAgent;
+
+abstract public class PubSubAgent extends AbstractAgent implements PublishingAgent, SubscribingAgent
+{
+}
---
+++ common/src/main/java/de/effms/jade/agent/publish/PublishingAgent.java
@@ -0,0 +1,8 @@
+package de.effms.jade.agent.publish;
+
+import de.effms.jade.agent.lifecycle.LifecycleProvidingAgent;
+import de.effms.jade.agent.service.ServiceProvidingAgent;
+
+public interface PublishingAgent extends LifecycleProvidingAgent, ServiceProvidingAgent
+{
+}
