1145be7680946a94e4e0c53ef820e85dffb77ffa
Fritz Gerneth <fgerneth@effms.de>
2014-03-17 17:59:23 +0100

    [de.effms.jade.commons] ServiceProvidingAgent extends Agent

--- common/src/main/java/de/effms/jade/agent/service/ServiceProvidingAgent.java
+++ common/src/main/java/de/effms/jade/agent/service/ServiceProvidingAgent.java
@@ -1,10 +1,11 @@
 package de.effms.jade.agent.service;
 
+import de.effms.jade.agent.Agent;
 import jade.content.lang.Codec;
 import jade.content.onto.Ontology;
 import jade.domain.FIPAAgentManagement.ServiceDescription;
 
-public interface ServiceProvidingAgent
+public interface ServiceProvidingAgent extends Agent
 {
     public void registerService(ServiceDescription serviceDescription, Ontology ontology);
 
