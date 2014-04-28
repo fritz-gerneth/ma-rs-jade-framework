4593bc396b7a8e11777e12c08db953513a41a86e
Fritz Gerneth <fgerneth@effms.de>
2014-04-28 09:34:34 +0200

    Add RecommenderSystem ontology

---
+++ common/src/main/java/de/effms/jade/ontology/RecommenderSystemOntology.java
@@ -0,0 +1,87 @@
+package de.effms.jade.ontology;
+
+import jade.content.abs.AbsObject;
+import jade.content.onto.BasicOntology;
+import jade.content.onto.Ontology;
+import jade.content.onto.OntologyException;
+import jade.content.onto.ReflectiveIntrospector;
+import jade.content.schema.AgentActionSchema;
+import jade.content.schema.ConceptSchema;
+import jade.content.schema.Facet;
+import jade.content.schema.ObjectSchema;
+import jade.content.schema.PrimitiveSchema;
+import jade.content.schema.TermSchema;
+
+public class RecommenderSystemOntology extends Ontology implements RecommenderSystemVocabulary
+{
+    private static RecommenderSystemOntology instance = new RecommenderSystemOntology();
+
+    public static RecommenderSystemOntology getInstance()
+    {
+        return RecommenderSystemOntology.instance;
+    }
+
+    private RecommenderSystemOntology()
+    {
+        super(RecommenderSystemVocabulary.NAME, new Ontology[] {BasicOntology.getInstance()}, new ReflectiveIntrospector());
+
+        try {
+            ConceptSchema recommenderConcept = new ConceptSchema(RecommenderSystemVocabulary.RECOMMENDER);
+
+            ConceptSchema userConcept = new ConceptSchema(RecommenderSystemVocabulary.USER);
+
+            ConceptSchema userId = new ConceptSchema(RecommenderSystemVocabulary.USER_IDENTITY);
+            userId.addSuperSchema(recommenderConcept);
+            userId.add(RecommenderSystemVocabulary.USER_NAME, (PrimitiveSchema) getSchema(BasicOntology.STRING));
+            userConcept.add(RecommenderSystemVocabulary.USER_UID, userId);
+
+            ConceptSchema agentConcept = new ConceptSchema(RecommenderSystemVocabulary.AGENT);
+            agentConcept.addSuperSchema(recommenderConcept);
+
+            ConceptSchema itemConcept = new ConceptSchema(RecommenderSystemVocabulary.ITEM);
+
+            final ConceptSchema actionConcept = new ConceptSchema(RecommenderSystemVocabulary.ACTION);
+            actionConcept.addSuperSchema(itemConcept);
+            actionConcept.add(RecommenderSystemVocabulary.ACTION_SUBJECT, new ConceptSchema(ConceptSchema.BASE_NAME));
+            actionConcept.add(RecommenderSystemVocabulary.ACTION_OBJECT, new ConceptSchema(ConceptSchema.BASE_NAME), ObjectSchema.OPTIONAL);
+
+            AgentActionSchema recommendsConcept = new AgentActionSchema(RecommenderSystemVocabulary.RECOMMENDS);
+            recommendsConcept.addSuperSchema(actionConcept);
+            recommendsConcept.addFacet(RecommenderSystemVocabulary.ACTION_OBJECT, new Facet()
+            {
+                @Override
+                public void validate(AbsObject value, Ontology onto) throws OntologyException
+                {
+                    ObjectSchema valueSchema = onto.getSchema(value.getTypeName());
+                    if (!valueSchema.isCompatibleWith(actionConcept)) {
+                        throw new OntologyException("Value " + value + " is not an " + RecommenderSystemVocabulary.ACTION);
+                    }
+                }
+            });
+            recommendsConcept.add(RecommenderSystemVocabulary.RECOMMENDS_REASON, new ConceptSchema(ConceptSchema.BASE_NAME), ObjectSchema.OPTIONAL);
+
+            ConceptSchema preferenceConcept = new ConceptSchema(RecommenderSystemVocabulary.PREFERENCE);
+            preferenceConcept.add(RecommenderSystemVocabulary.PREFERENCE_WHAT, (TermSchema) getSchema(TermSchema.BASE_NAME));
+            preferenceConcept.add(RecommenderSystemVocabulary.PREFERENCE_RATING, (PrimitiveSchema) getSchema(BasicOntology.INTEGER));
+            userConcept.add(RecommenderSystemVocabulary.PREFERS, preferenceConcept, 0, ObjectSchema.UNLIMITED);
+
+            ConceptSchema situationConcept = new ConceptSchema(RecommenderSystemVocabulary.SITUATION);
+            userConcept.add(RecommenderSystemVocabulary.SITUATED, situationConcept, 0, ObjectSchema.UNLIMITED);
+
+            this.add(recommenderConcept);
+            this.add(userConcept);
+            this.add(userId);
+            this.add(agentConcept);
+
+            this.add(itemConcept);
+            this.add(actionConcept);
+
+            this.add(recommendsConcept);
+            this.add(preferenceConcept);
+            this.add(situationConcept);
+        } catch (OntologyException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ common/src/main/java/de/effms/jade/ontology/RecommenderSystemVocabulary.java
@@ -0,0 +1,42 @@
+package de.effms.jade.ontology;
+
+public interface RecommenderSystemVocabulary
+{
+    public static String NAME = "de.effms.ontology.RecommenderSystem";
+
+    public static String USER = "rs_user";
+
+    public static String USER_UID = "rs_user_uid";
+
+    public static String USER_IDENTITY = "rs_user_identity";
+
+    public static String USER_NAME = "rs_user_name";
+
+    public static String AGENT = "rs_agent";
+
+    public static String RECOMMENDER = "rs_recommender";
+
+    public static String ITEM = "rs_item";
+
+    public static String ACTION = "rs_action";
+
+    public static String ACTION_SUBJECT = "rs_action_subject";
+
+    public static String ACTION_OBJECT = "rs_action_object";
+
+    public static String RECOMMENDS = "rs_recommends";
+
+    public static String RECOMMENDS_REASON = "rs_reason";
+
+    public static String PREFERS = "rs_prefers";
+
+    public static String PREFERENCE = "rs_preference";
+
+    public static String PREFERENCE_WHAT = "rs_preference_what";
+
+    public static String PREFERENCE_RATING = "rs_preference_rating";
+
+    public static String SITUATED = "rs_situated";
+
+    public static String SITUATION = "rs_situation";
+}
