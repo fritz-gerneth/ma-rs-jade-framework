a5252997e5371e679e8663f81ad43c271d5f6d5d
Fritz Gerneth <fgerneth@effms.de>
2014-05-01 13:49:30 +0200

    Add new ontology to express relations between concepts

---
+++ common/src/main/java/de/effms/jade/ontology/RelationalOntology.java
@@ -0,0 +1,42 @@
+package de.effms.jade.ontology;
+
+import jade.content.onto.BasicOntology;
+import jade.content.onto.Ontology;
+import jade.content.onto.OntologyException;
+import jade.content.schema.*;
+
+public class RelationalOntology extends Ontology implements RelationalVocabulary
+{
+    private static RelationalOntology instance = new RelationalOntology();
+
+    public static RelationalOntology getInstance()
+    {
+        return RelationalOntology.instance;
+    }
+
+    private RelationalOntology()
+    {
+        super(RecommenderSystemVocabulary.NAME, BasicOntology.getInstance());
+
+        try {
+            PredicateSchema doesPredicate = new PredicateSchema(DOES);
+            doesPredicate.add(DOES_WHO, getSchema(ConceptSchema.BASE_NAME));
+            doesPredicate.add(DOES_WHAT, getSchema(ConceptSchema.BASE_NAME));
+
+            PredicateSchema isPredicate = new PredicateSchema(IS);
+            isPredicate.add(IS_WHO, getSchema(ConceptSchema.BASE_NAME));
+            isPredicate.add(IS_WHAT, getSchema(ConceptSchema.BASE_NAME));
+
+            PredicateSchema hasPredicate = new PredicateSchema(HAS);
+            hasPredicate.add(HAS_WHO, getSchema(ConceptSchema.BASE_NAME));
+            hasPredicate.add(HAS_WHAT, getSchema(ConceptSchema.BASE_NAME));
+
+            this.add(doesPredicate);
+            this.add(isPredicate);
+            this.add(hasPredicate);
+        } catch (OntologyException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ common/src/main/java/de/effms/jade/ontology/RelationalVocabulary.java
@@ -0,0 +1,24 @@
+package de.effms.jade.ontology;
+
+public interface RelationalVocabulary
+{
+    public static String NAME = "de.effms.ontology.Relational";
+
+    public static String DOES = "rel_does";
+
+    public static String DOES_WHO = "rel_does_who";
+
+    public static String DOES_WHAT = "rel_does_what";
+
+    public static String IS = "rel_is";
+
+    public static String IS_WHO = "rel_is_who";
+
+    public static String IS_WHAT = "rel_is_what";
+
+    public static String HAS = "rel_has";
+
+    public static String HAS_WHO = "rel_has_who";
+
+    public static String HAS_WHAT = "rel_has_what";
+}
